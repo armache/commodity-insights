74a6603660b485f310368e5e65d3eaa144c63cc8
Ara Martirosyan <ara@travelich.com>
2022-04-07 17:17:58 +0100

    renamed project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/anglo-american-ui'),
+      dir: require('path').join(__dirname, './coverage/commodity-insights'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'anglo-american-ui'`, () => {
+  it(`should have as title 'commodity-insights'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    //expect(app.title).toEqual('anglo-american-ui');
+    //expect(app.title).toEqual('commodity-insights');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('anglo-american-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('commodity-insights app is running!');
   });
 });
